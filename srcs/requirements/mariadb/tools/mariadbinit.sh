3227c028addf404db355c3ecd2552614d9817492
Reinan Vieira <92590079+reinanmat@users.noreply.github.com>
2024-03-12 18:19:04 -0300

    refactor: update

--- srcs/requirements/mariadb/tools/mariadbinit.sh
+++ srcs/requirements/mariadb/tools/mariadbinit.sh
@@ -6,5 +6,4 @@ mariadb -u root -e \
 	"CREATE DATABASE IF NOT EXISTS ${DB_NAME}; \
 	CREATE USER '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}'; \
 	GRANT ALL ON ${DB_NAME}.* TO '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}'; \
-	FLUSH PRIVILEGES;" && \
-	service mariadb stop
+	FLUSH PRIVILEGES;"
--- srcs/requirements/wordpress/tools/wordpressinit.sh
+++ srcs/requirements/wordpress/tools/wordpressinit.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-wp core install --allow-root \
-	--path=/var/www/html \
-	--url=reviera.42.fr \
-	--title="Inception" \
-	--admin_user='admin' \
-	--admin_password='admin' \
-	--admin_email='revieira@student.42sp.org.br'
+if [ -f /var/www/html/wp-config-sample.php ]; then
+	rm -rf /var/www/html/wp-config-sample.php
+	wp core install --allow-root \
+		--path=/var/www/html \
+		--url=reviera.42.fr \
+		--title="Inception" \
+		--admin_user='admin' \
+		--admin_password='admin' \
+		--admin_email='revieira@student.42sp.org.br'
 
-wp user create --allow-root	\
-	--path=/var/www/html \
-	'test' 'test@test.com' \
-	--user_pass='test'
+	wp user create --allow-root	\
+		--path=/var/www/html \
+		'test' 'test@test.com' \
+		--user_pass='test'
+fi
 
 exec php-fpm7.4 -F
