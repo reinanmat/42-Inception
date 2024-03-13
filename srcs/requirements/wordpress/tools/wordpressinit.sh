5b9c2cebb2cfa6e679dae2d87848422637749af9
Reinan Vieira <92590079+reinanmat@users.noreply.github.com>
2024-03-13 17:09:51 -0300

    refactor: env variables

--- srcs/requirements/wordpress/tools/wordpressinit.sh
+++ srcs/requirements/wordpress/tools/wordpressinit.sh
@@ -4,16 +4,17 @@ if [ -f /var/www/html/wp-config-sample.php ]; then
 	rm -rf /var/www/html/wp-config-sample.php
 	wp core install --allow-root \
 		--path=/var/www/html \
-		--url=revieira.42.fr \
 		--title="Inception" \
-		--admin_user='admin' \
-		--admin_password='admin' \
-		--admin_email='revieira@student.42sp.org.br'
+		--url=$DOMAIN_NAME \
+		--admin_user=$WP_ROOT_USER \
+		--admin_password=$WP_ROOT_PASSWORD \
+		--admin_email=$WP_ROOT_EMAIL
 
 	wp user create --allow-root	\
 		--path=/var/www/html \
-		'test' 'test@test.com' \
-		--user_pass='test'
+		"$WP_USER" "$WP_EMAIL" \
+		--user_pass=$WP_PASSWORD \
+		--role='author'
 fi
 
 exec php-fpm7.4 -F
