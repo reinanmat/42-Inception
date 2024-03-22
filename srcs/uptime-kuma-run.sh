d8c005417ad475b4ac55ffbf8b5e69f7023901f5
Reinan Vieira <92590079+reinanmat@users.noreply.github.com>
2024-03-22 18:54:59 -0300

    refactor: removed uptime-kuma script

--- srcs/uptime-kuma-run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker run -d --restart=always -p 3001:3001 \
-			-v /var/run/docker.sock:/var/run/docker.sock -v uptime-kuma:/app/data \
-			--name uptime-kuma louislam/uptime-kuma:1
